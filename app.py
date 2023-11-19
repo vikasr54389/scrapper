ded8bb6a613652c0efd0116e7305c8b5e3ab390e
vikasr54389 <126090080+vikasr54389@users.noreply.github.com>
2023-11-19 23:23:31 +0530

    app.py

--- app.py
+++ app.py
@@ -73,12 +73,6 @@ def index():
                           "Comment": custComment}
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
-
-            
-            ''' client = pymongo.MongoClient("mongodb+srv://pwskills:pwskills@cluster0.ln0bt5m.mongodb.net/?retryWrites=true&w=majority")
-            db =client['scrapper_eng_pwskills']
-            coll_pw_eng = db['scraper_pwskills_eng']
-            coll_pw_eng.insert_many(reviews)'''
             
         
 
